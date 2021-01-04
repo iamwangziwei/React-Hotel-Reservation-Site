2784d60225bdeabc9dc61724864643a6572ca42b
wangziwei <35870166+maoyiyiyi@users.noreply.github.com>
2021-01-03 22:16:40 -0500

    modify home and error page

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                single room page
+            </div>
+        )
+    }
+}
+
