84ff78f56253c34385a6a310a5e80c09ed0ef435
wangziwei <35870166+maoyiyiyi@users.noreply.github.com>
2021-01-04 22:11:13 -0500

    show the title of each room when click on features

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import defaultbcg from '../images/room-1.jpeg'
+import Hero from '../Components/Hero'
+import Banner from '../Components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from '../Context'
+
 
 export default class SingleRoom extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            slug:this.props.match.params.slug,
+            defaultbcg
+        }
+    }
+    
+    static contextType = RoomContext;
+    //componentDidMount(){}
+
     render() {
-        return (
-            <div>
-                single room page
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug)
+
+        if(!room){
+            return (<div className="error">
+                <h3>no such room..</h3>
+                <Link to='./room' className="btn-primary">
+                    back to rooms
+                </Link>
             </div>
+            );
+        }
+
+        const {name,description,capacity,size,price,extras,breakfast,pets,images} = room
+        return (
+            <Hero hero='roomsHero'>
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className='btn-primary'>
+                        back to rooms
+                    </Link>
+                </Banner>
+            </Hero>
         )
     }
 }
